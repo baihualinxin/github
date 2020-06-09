9d07901de865de4283f705bea98e0c3052143cec
baihualinxin <baihualinxin@github.com>
2020-06-09 17:20:37 +0800

    创建分支VC

---
+++ github/branchViewController.m
@@ -0,0 +1,32 @@
+//
+//  branchViewController.m
+//  github
+//
+//  Created by DCHealth on 2020/6/9.
+//  Copyright © 2020 DCHealth. All rights reserved.
+//
+
+#import "branchViewController.h"
+
+@interface branchViewController ()
+
+@end
+
+@implementation branchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+      NSLog(@"分支2222");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
